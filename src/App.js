1ee43cacc31bea498f8b4f212690fcb2b3bb484a
fibonacci2012 <infodomalien@gmail.com>
2023-04-05 15:51:43 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import imgLogo from "./img/logo_dots_black.png";
+import imgTelegram from "./img/telegram.png";
+import imgInstagram from "./img/instagram.png";
+import pdfPricelist from "./rsc/Roof-price.pdf";
 import './App.css';
 
 function App() {
+  const [shadow, setShadow] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    const xPos = e.clientX;
+    const yPos = e.clientY;
+    const offsetX = e.currentTarget.offsetLeft;
+    const offsetY = e.currentTarget.offsetTop;
+    const xShadow = Math.round((xPos - offsetX - e.currentTarget.offsetWidth / 2) / 10);
+    const yShadow = Math.round((yPos - offsetY - e.currentTarget.offsetHeight / 2) / 10);
+    setShadow({ x: xShadow, y: yShadow });
+  };
+  // Comm for commit
+  const downloadFile = () => {
+  const url = './rsc/Roof-price.pdf';
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      const url = window.URL.createObjectURL(new Blob([blob]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'Price List.pdf');
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    });
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onMouseMove={handleMouseMove}>
+      <div className="logo-container" style={{ boxShadow: `${shadow.x}px ${shadow.y}px 10px rgba(0, 0, 0, 0.5)` }}>
+        <img src={imgLogo} alt="Logo" />
+      </div>
+      <div className="button-container">
+        <button onClick={downloadFile}>Price List</button>
+        <button onClick={() => window.location.href="https://t.me/roofspecialty"}><img src={imgTelegram}/></button>
+        <button onClick={() => window.location.href="https://www.instagram.com/roof.specialty.coffee/"}><img src={imgInstagram}/></button>
+      </div>
     </div>
   );
 }
